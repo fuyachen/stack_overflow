333b5fa148cd53e0671b027570921f40119d7ef4
achen <294143413@qq.com>
2024-02-19 22:19:30 +0800

    implement base tailwindcss config and style system

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <main className="flex text-2xl">Hello，Next.js！</main>
+  return (
+    <main className="flex-center flex-col">
+      <h1 className="h1-bold">Heading one</h1>
+      <h2 className="h2-bold">Heading one</h2>
+      <h3 className="h3-bold">Heading one</h3>
+    </main>
+  )
 }
